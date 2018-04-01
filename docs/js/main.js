9d823cb5757379cdf0363354b1074b0db40c7b3f
Nader Namini-Asl <nadernamini@berkeley.edu>
2018-03-31 20:14:02 -0700

    added website files

---
+++ docs/js/main.js
@@ -0,0 +1,454 @@
+$(document).ready(function () {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+    $("#cf1_controls").on('click', 'span', function () {
+        $("#cf1 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf1 img").eq(newImage).addClass("opaque");
+
+        $("#cf1_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+    $("#cf2_controls").on('click', 'span', function () {
+        $("#cf2 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf2 img").eq(newImage).addClass("opaque");
+
+        $("#cf2_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf3_controls").on('click', 'span', function () {
+        $("#cf3 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf3 img").eq(newImage).addClass("opaque");
+
+        $("#cf3_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf4_controls").on('click', 'span', function () {
+        $("#cf4 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf4 img").eq(newImage).addClass("opaque");
+
+        $("#cf4_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf5_controls").on('click', 'span', function () {
+        $("#cf5 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf5 img").eq(newImage).addClass("opaque");
+
+        $("#cf5_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf6_controls").on('click', 'span', function () {
+        $("#cf6 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf6 img").eq(newImage).addClass("opaque");
+
+        $("#cf6_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf7_controls").on('click', 'span', function () {
+        $("#cf7 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf7 img").eq(newImage).addClass("opaque");
+
+        $("#cf7_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf8_controls").on('click', 'span', function () {
+        $("#cf8 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf8 img").eq(newImage).addClass("opaque");
+
+        $("#cf8_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf9_controls").on('click', 'span', function () {
+        $("#cf9 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf9 img").eq(newImage).addClass("opaque");
+
+        $("#cf9_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf10_controls").on('click', 'span', function () {
+        $("#cf10 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf10 img").eq(newImage).addClass("opaque");
+
+        $("#cf10_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf11_controls").on('click', 'span', function () {
+        $("#cf11 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf11 img").eq(newImage).addClass("opaque");
+
+        $("#cf11_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf12_controls").on('click', 'span', function () {
+        $("#cf12 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf12 img").eq(newImage).addClass("opaque");
+
+        $("#cf12_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf13_controls").on('click', 'span', function () {
+        $("#cf13 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf13 img").eq(newImage).addClass("opaque");
+
+        $("#cf13_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf14_controls").on('click', 'span', function () {
+        $("#cf14 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf14 img").eq(newImage).addClass("opaque");
+
+        $("#cf14_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf15_controls").on('click', 'span', function () {
+        $("#cf15 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf15 img").eq(newImage).addClass("opaque");
+
+        $("#cf15_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf16_controls").on('click', 'span', function () {
+        $("#cf16 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf16 img").eq(newImage).addClass("opaque");
+
+        $("#cf16_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf17_controls").on('click', 'span', function () {
+        $("#cf17 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf17 img").eq(newImage).addClass("opaque");
+
+        $("#cf17_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf18_controls").on('click', 'span', function () {
+        $("#cf18 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf18 img").eq(newImage).addClass("opaque");
+
+        $("#cf18_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf19_controls").on('click', 'span', function () {
+        $("#cf19 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf19 img").eq(newImage).addClass("opaque");
+
+        $("#cf19_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf20_controls").on('click', 'span', function () {
+        $("#cf20 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf20 img").eq(newImage).addClass("opaque");
+
+        $("#cf20_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf21_controls").on('click', 'span', function () {
+        $("#cf21 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf21 img").eq(newImage).addClass("opaque");
+
+        $("#cf21_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf22_controls").on('click', 'span', function () {
+        $("#cf22 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf22 img").eq(newImage).addClass("opaque");
+
+        $("#cf22_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf23_controls").on('click', 'span', function () {
+        $("#cf23 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf23 img").eq(newImage).addClass("opaque");
+
+        $("#cf23_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf24_controls").on('click', 'span', function () {
+        $("#cf24 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf24 img").eq(newImage).addClass("opaque");
+
+        $("#cf24_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf25_controls").on('click', 'span', function () {
+        $("#cf25 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf25 img").eq(newImage).addClass("opaque");
+
+        $("#cf25_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf26_controls").on('click', 'span', function () {
+        $("#cf26 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf26 img").eq(newImage).addClass("opaque");
+
+        $("#cf26_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf27_controls").on('click', 'span', function () {
+        $("#cf27 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf27 img").eq(newImage).addClass("opaque");
+
+        $("#cf27_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf28_controls").on('click', 'span', function () {
+        $("#cf28 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf28 img").eq(newImage).addClass("opaque");
+
+        $("#cf28_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf29_controls").on('click', 'span', function () {
+        $("#cf29 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf29 img").eq(newImage).addClass("opaque");
+
+        $("#cf29_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf30_controls").on('click', 'span', function () {
+        $("#cf30 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf30 img").eq(newImage).addClass("opaque");
+
+        $("#cf30_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf31_controls").on('click', 'span', function () {
+        $("#cf31 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf31 img").eq(newImage).addClass("opaque");
+
+        $("#cf31_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf32_controls").on('click', 'span', function () {
+        $("#cf32 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf32 img").eq(newImage).addClass("opaque");
+
+        $("#cf32_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf33_controls").on('click', 'span', function () {
+        $("#cf33 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf33 img").eq(newImage).addClass("opaque");
+
+        $("#cf33_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf34_controls").on('click', 'span', function () {
+        $("#cf34 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf34 img").eq(newImage).addClass("opaque");
+
+        $("#cf34_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf35_controls").on('click', 'span', function () {
+        $("#cf35 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf35 img").eq(newImage).addClass("opaque");
+
+        $("#cf35_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf36_controls").on('click', 'span', function () {
+        $("#cf36 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf36 img").eq(newImage).addClass("opaque");
+
+        $("#cf36_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf37_controls").on('click', 'span', function () {
+        $("#cf37 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf37 img").eq(newImage).addClass("opaque");
+
+        $("#cf37_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf38_controls").on('click', 'span', function () {
+        $("#cf38 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf38 img").eq(newImage).addClass("opaque");
+
+        $("#cf38_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf39_controls").on('click', 'span', function () {
+        $("#cf39 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf39 img").eq(newImage).addClass("opaque");
+
+        $("#cf39_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#cf40_controls").on('click', 'span', function () {
+        $("#cf40 img").removeClass("opaque");
+
+        var newImage = $(this).index();
+
+        $("#cf40 img").eq(newImage).addClass("opaque");
+
+        $("#cf40_controls span").removeClass("selected");
+        $(this).addClass("selected");
+    });
+});
