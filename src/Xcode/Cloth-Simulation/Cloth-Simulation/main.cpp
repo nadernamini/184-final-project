ac0f1638beb621c673d94722378bc1f52c69168d
Marc WuDunn <MarcWuDunn@airbears2-10-142-73-10.airbears2.1918.berkeley.edu>
2018-04-13 18:11:01 -0700

    xcode

---
+++ src/Xcode/Cloth-Simulation/Cloth-Simulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cloth-Simulation
+//
+//  Created by Marc WuDunn on 4/13/18.
+//  Copyright © 2018 Marc WuDunn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
